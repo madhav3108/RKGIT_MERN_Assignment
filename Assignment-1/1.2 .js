6a26817e43a3dbd74a11f8a5b1c162c691d6dfb2
Madhav Singh <88900164+madhav3108@users.noreply.github.com>
2023-07-03 09:34:59 +0530

    Update 1.2 .js

--- Assignment-1/1.2 .js
+++ Assignment-1/1.2 .js
@@ -1,27 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\pardirnatural\partightenfactor0
+//2.Write a JavaScript program to get the current date.
+//Expected Output :
+//mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 
-\f0\fs24 \cf0 2.Write a JavaScript program to get the current date.\
-Expected Output :\
-mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy\
-\
-=======================================\
-\
-\pard\pardeftab720\pardirnatural\partightenfactor0
+function GETDATE(){
+  const d = new Date();
+  var m=d.getMonth();
+  var y=d.getMonth();
+  var date=d.getDate();
+  const today=`$\{m\} / $\{date\} / $\{y\} , $\{m\} - $\{date\} - $\{y\} `; console.log(today);}
 
-\f1\fs29 \cf0 \expnd0\expndtw0\kerning0
-function GETDATE()\{\
-\'a0\'a0\'a0 const d = new Date();\
-\'a0\'a0\'a0 var m=d.getMonth();\
-\'a0\'a0\'a0 var y=d.getMonth();\
-\'a0\'a0\'a0 var date=d.getDate();\
-\'a0\'a0\'a0 const today=`$\{m\} / $\{date\} / $\{y\} , $\{m\} - $\{date\} - $\{y\} `; console.log(today);\}\
-\
-console.log(GETDATE());\
-VM5375:6        6 / 1 / 6      ,           6 - 1 - 6 \
+console.log(GETDATE());
+      6 / 1 / 6      ,           6 - 1 - 6 
 }
